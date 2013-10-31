15f4a4a9d235d8479f1a0a8a9ec343c61ba5176b
Zmo <simo.kolppo@gmail.com>
2013-10-31 11:29:25 +0200

    Second week excercises halfway through

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+gem 'debugger', :group => :development
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,14 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    ratings.inject(0) { |result, element| result + element.score } / ratings.size
+    #sum = 0
+    #ratings.each do |rating|
+    #  sum = sum + rating.score
+    #end
+    #sum = sum / ratings.size
+    #return sum
+  end
 end
