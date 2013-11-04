6005a7177f3615b856d0fd6b3f63346c7f8838d5
Zmo <simo.kolppo@gmail.com>
2013-11-04 23:14:02 +0200

    start of week 3

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,8 @@
 class Brewery < ActiveRecord::Base
   has_many :beers
+  has_many :ratings, :through => :beers
+
+  def average_rating
+    ratings.inject(0) { |result, element| result + element.score } / ratings.size
+  end
 end
